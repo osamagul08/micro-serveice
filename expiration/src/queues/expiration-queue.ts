ac03457ac97179e4823a99041ebf5c89c63ac7c6
usama gul <usama.gul@transconomy.com>
2025-05-15 15:18:34 +0500

    add publisher in expiration service

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,21 @@
+import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats-wrapper";
+
+interface PayLoad {
+  orderId: string;
+}
+const expirationQueue = new Queue<PayLoad>("expiration", {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log("Expiration job processing", job.data);
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
+});
+
+export { expirationQueue };
