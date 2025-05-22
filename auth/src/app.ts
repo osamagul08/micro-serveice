2df92fab665267ef2795b82ff0d13aeb53abb15d
usama gul <usama.gul@transconomy.com>
2025-05-22 17:34:10 +0500

    minor changes

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -15,10 +15,9 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    secure: process.env.NODE_ENV !== "test" ? true : false,
   })
 );
-
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
