3aee38e57bc082a3105c94f523942bfd9f10fcfd
usama gul <usama.gul@transconomy.com>
2025-05-22 13:12:37 +0500

    update common module

--- common/src/events/types/order-status.ts
+++ common/src/events/types/order-status.ts
@@ -1,8 +1,6 @@
 export enum OrderStatus {
-  Created = "created", //order has been created but not yet paid for
-  AwaitingPayment = "awaiting:payment", //order has been created and is awaiting payment
-  Complete = "complete", //order has been paid for and is complete
-  //the order has been cancelled by the user or the system
-  Cancelled = "cancelled", //the ticket the order is reserving has been cancelled
-  Expired = "expired", //the order has expired and is no longer valid
+  Created = "created",
+  AwaitingPayment = "awaiting:payment",
+  Complete = "complete",
+  Cancelled = "cancelled",
 }
