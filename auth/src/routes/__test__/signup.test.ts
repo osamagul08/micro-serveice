736fb4adc0cba35c9da22564ca90116d962653ff
usama gul <usama.gul@transconomy.com>
2025-06-03 11:56:16 +0500

    revert changes

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it.skip("returns a 201 on successful signup", async () => {
+it("returns a 201 on successful signup", async () => {
   return request(app)
     .post("/api/users/signup")
     .send({
